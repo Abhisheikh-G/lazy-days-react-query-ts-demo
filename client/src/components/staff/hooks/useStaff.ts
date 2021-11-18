77568b99f53e96bc25e835429ecf20dde3cf401e
Abhisheikh Gill <abhisheikhg@gmail.com>
2021-11-18 18:08:51 -0500

    Initial commit

---
+++ client/src/components/staff/hooks/useStaff.ts
@@ -0,0 +1,34 @@
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
+import { useQuery } from 'react-query';
+
+import type { Staff } from '../../../../../shared/types';
+import { axiosInstance } from '../../../axiosInstance';
+import { queryKeys } from '../../../react-query/constants';
+import { filterByTreatment } from '../utils';
+
+// for when we need a query function for useQuery
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
+
+interface UseStaff {
+  staff: Staff[];
+  filter: string;
+  setFilter: Dispatch<SetStateAction<string>>;
+}
+
+export function useStaff(): UseStaff {
+  // for filtering staff by treatment
+  const [filter, setFilter] = useState('all');
+
+  const fallback = [];
+  const selectFn = useCallback((data) => filterByTreatment(data, filter), [
+    filter,
+  ]);
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff, {
+    select: filter === 'all' ? undefined : selectFn,
+  });
+
+  return { staff, filter, setFilter };
+}
---
+++ client/src/components/user/hooks/useUser.ts
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import { useQuery, useQueryClient } from 'react-query';
+
+import type { User } from '../../../../../shared/types';
+import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
+import { queryKeys } from '../../../react-query/constants';
+import {
+  clearStoredUser,
+  getStoredUser,
+  setStoredUser,
+} from '../../../user-storage';
+
+async function getUser(user: User | null): Promise<User | null> {
+  if (!user) return null;
+  const { data } = await axiosInstance.get(`/user/${user.id}`, {
+    headers: getJWTHeader(user),
+  });
+  return data.user;
+}
+
+interface UseUser {
+  user: User | null;
+  updateUser: (user: User) => void;
+  clearUser: () => void;
+}
+
+export function useUser(): UseUser {
+  const [user, setUser] = useState<User | null>(getStoredUser());
+  const queryClient = useQueryClient();
+  useQuery(queryKeys.user, () => getUser(user), {
+    enabled: !!user,
+    onSuccess: (data) => setUser(data),
+  });
+
+  // meant to be called from useAuth
+  function updateUser(newUser: User): void {
+    // set user in state
+    setUser(newUser);
+
+    // update user in localstorage
+    setStoredUser(newUser);
+
+    // pre-populate user profile in React Query client
+    queryClient.setQueryData(queryKeys.user, newUser);
+  }
+
+  // meant to be called from useAuth
+  function clearUser() {
+    // update state
+    setUser(null);
+
+    // remove from localstorage
+    clearStoredUser();
+
+    // reset user to null in query client
+    queryClient.setQueryData(queryKeys.user, null);
+
+    // remove user appointments query
+    queryClient.removeQueries([queryKeys.appointments, queryKeys.user]);
+  }
+
+  return { user, updateUser, clearUser };
+}
